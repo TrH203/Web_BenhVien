a7fb905dfafd69d9c04b763fd7bf0c39d9b179de
TrH203 <hoangtrhien203@gmail.com>
2023-12-17 23:47:07 +0700

    add hash password to Sign Up service

--- src/services/crudService.js
+++ src/services/crudService.js
@@ -1,8 +1,23 @@
-let createNewService = (data) => {
+import bcrypt from "bcrypt";
+
+
+
+let createNewService = async (data) => {
+    let passwordHashed = await hashPassword(data.password);
     console.log("Data from crudService");
     console.log(data);
+    console.log(passwordHashed);
 }
 
+let hashPassword = async (password) => {
+    try {
+        const salt = bcrypt.genSaltSync(10); // get Salt (how to hash)
+        let hash = await bcrypt.hash(password, salt);
+        return hash;
+    } catch (e) {
+        return e;
+    }
+}
 
 module.exports = {
     createNewService: createNewService,
