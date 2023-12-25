2d85fff0c903a147343b287d0eb7ec3400320e9f
TrH203 <hoangtrhien203@gmail.com>
2023-12-25 11:59:14 +0700

    Update error status code in handleLogin function

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -4,7 +4,7 @@ let handleLogin = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     if (!email || !password) { // if (email === undefined || email === "") {
-        return res.status(500).json({
+        return res.status(200).json({
             errCode: 1,
             message: "Missing value",
             user: {},
@@ -13,7 +13,7 @@ let handleLogin = async (req, res) => {
     let userData = await Service.handleUserLogin(email, password);
     userData.user = userData.user ? userData.user : {} // if userData.user is none => userData.user = {}
     if (userData.errCode === 1) {
-        return res.status(500).json(userData);
+        return res.status(200).json(userData);
     }
     else {
         return res.status(200).json(userData);
