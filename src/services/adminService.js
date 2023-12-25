ed76e7d886bf14dd3c2dc47e067b780dad57552b
TrH203 <hoangtrhien203@gmail.com>
2023-12-25 15:30:32 +0700

    update get user API

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -2,32 +2,47 @@ import db from "../models/index";
 
 const getAllUsers = () => {
     return new Promise(async (resolve, reject) => {
+        let returnData = { users: {} };
         try {
             let users = await db.Users.findAll({
-                attributes: ["id", "email", "firstName", "lastName", "phoneNumber"],
+                // attributes: ["id", "email", "firstName", "lastName", "phoneNumber"],
+                attributes: { exclude: ["password"] },
                 raw: true,
             })
-            resolve(users);
+            returnData.errCode = 0;
+            returnData.message = "get all users";
+            returnData.users = users;
+            resolve(returnData);
         } catch (e) {
-            reject(e);
+            returnData.errCode = 4;
+            returnData.message = e;
+            reject(returnData);
         }
     })
 }
 const get1User = (id) => {
     return new Promise(async (resolve, reject) => {
+        let returnData = { users: {} };
         try {
             let user = await db.Users.findByPk(id, {
-                attributes: ["id", "email", "firstName", "lastName", "phoneNumber"],
+                attributes: { exclude: ["password"] },
                 raw: true,
             })
             if (user) {
-                resolve(user);
+                returnData.errCode = 0;
+                returnData.message = "get one user";
+                returnData.users = user;
+                resolve(returnData);
             }
             else {
-                resolve({})
+                returnData.errCode = 5;
+                returnData.message = "not found user";
+                resolve(returnData);
             }
         } catch (e) {
-            reject(e);
+            returnData.errCode = 0;
+            returnData.message = e;
+            reject(returnData);
         }
     })
 }
